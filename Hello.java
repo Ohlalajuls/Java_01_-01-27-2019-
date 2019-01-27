1f938c2fd8484ed848a78d634777daa85bcdd609
Rokuto <gauven.boro@gmail.com>
2019-01-27 15:33:11 +0800

    Updated Hello.java

--- Hello.java
+++ Hello.java
@@ -4,6 +4,11 @@ public class Hello {
   **/
   public static void main( String[] args ) {
     // prints the string "Hello World" on the screen
+
+  	int age = 22;
+  	char middleInitial = 'M';
+  	String name = "Gauven Roy Pascua";
+
     System.out.println("Hello");
     System.out.println("World");
   }
