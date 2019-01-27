ac8ba7afe985d1934d452427e22ba6fd407ea88f
Rokuto <gauven.boro@gmail.com>
2019-01-27 15:31:43 +0800

    Updated Hello.java

--- Hello.java
+++ Hello.java
@@ -4,6 +4,7 @@ public class Hello {
   **/
   public static void main( String[] args ) {
     // prints the string "Hello World" on the screen
-    System.out.println("Hello World");
+    System.out.println("Hello");
+    System.out.println("World");
   }
 }
